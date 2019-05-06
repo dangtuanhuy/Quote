7a3fb31775d6b67ddf1a2145f1df0a58cad63c8e
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-06 15:37:43 +0700

    Slot and Using Slot

---
+++ src/components/Quote.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="quote">
+    <div class="title quote">
+        <slot name="title"></slot>
+        <span style="color: green"><slot name="subtitle">The Subtitle</slot></span>
+    </div>
+    <div class="quote">
+        <slot ></slot>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.quote {
+        border: 1px solid #ccc;
+        box-shadow: 1px 1px 2px black;
+        padding: 30px;
+        margin: 30px auto;
+        text-align: center;
+    }
+
+    h2 {
+        color: red;
+    }
+
+    .title {
+        font-style: italic;
+    }
+</style>
